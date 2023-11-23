0efc462921997274ddfa11ebca36ecc925dcbbe9
Arpan Saha <arpansaha2k2@gmail.com>
2023-11-24 00:28:51 +0530

    first commit

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,38 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict' ;
+const api_key = "9eb7baa34b089a4490eaea7d620b25f6" ;
+
+/**
+ * Fetch data from server
+ * @param {String} URL API url
+ * @param {Function} callback callback
+ */
+export const fetchData = function(URL, callback) {
+  fetch(`${URL}&appid=${api_key}`) 
+  .then(res => res.json())
+  .then(data => callback(data)); 
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * @param {string} query Search query e.g. : "Delhi", "Mumbai"
+     */
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
